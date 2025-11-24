e4c877f3c242129fcde142ffeda75d5e83eaa71f
Nachiket Kapre <nachiket.kapre@uwaterloo.ca>
2025-11-23 22:37:26 -0500

    Creating content

---
+++ env.sh
@@ -0,0 +1,7 @@
+export LM_LICENSE_FILE=27000@droplet.eng.uwaterloo.ca
+export SALT_LICENSE_FILE=27000@droplet.eng.uwaterloo.ca
+export SALT_LICENSE_SERVER=27000@droplet.eng.uwaterloo.ca
+
+export PATH=/zfsspare/opt/Altera/altera_pro/25.1.1/quartus/bin:${PATH}
+export PATH=/usr/local/bin:${PATH}
+export PATH=/zfsspare/opt/oss-cad-suite/bin/:$PATH
