294e2fda378ffa9f4537e5c303f2279ebf28209d
Nachiket Kapre <nachiket.kapre@uwaterloo.ca>
2025-11-23 22:37:14 -0500

    Creating content

---
+++ rtl/naiveadder2048b.sv
@@ -0,0 +1,14 @@
+module naiveadder2048b
+#(
+    parameter W = 2048 // DO NOT CHANGE!
+) (
+    input  logic          clk,
+    input  logic          rst,
+    input  logic [W-1:0]  a,
+    input  logic [W-1:0]  b,
+    input logic           c_in,
+    output logic [W-1:0]  s,
+    output logic          c_out
+);
+
+endmodule
